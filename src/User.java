de95a322f97be927460abfe67efe35eb1aeef53d
[Tobias Rauschmaier] <tobias.rauschmaier@t-online.de>
2023-11-05 23:53:13 +0100

    updated user

--- src/User.java
+++ src/User.java
@@ -2,7 +2,17 @@ import java.util.ArrayList;
 
 public class User {
     private int id;
-    private String username;
+    private boolean login=false;
+    public boolean getLogin() {
+		return login;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setLogin(boolean login) {
+		this.login = login;
+	}
+	private String username;
     private String password;
     private ArrayList<String> warenkorb=new ArrayList<>();
     public User(int id, String username) {
@@ -14,7 +24,10 @@ public class User {
         this.password=password;
     }
     
-    public String getPassword() {
+    public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getPassword() {
     	return this.password;
     }
     // Getter und Setter für die Eigenschaften id und username
@@ -40,5 +53,8 @@ public class User {
     	for(String i:warenkorb) {
     		System.out.println(i);
     	}
+    }
+    public void setWarenkorb(ArrayList<String> warenkorb) {
+    	this.warenkorb=warenkorb;
     }
 }
